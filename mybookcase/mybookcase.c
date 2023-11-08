83a950e6818875ad2a935fc6a2bca9825f9fe548
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-11-08 20:21:28 +0300

    main process template created

--- mybookcase/mybookcase.c
+++ mybookcase/mybookcase.c
@@ -2,5 +2,45 @@
 
 int main()
 {
-	
+	while (1)
+	{
+		int process;
+		
+		printf("Please select the part you want to process: \n");
+		printf(" 1-Author\n 2-Book\n 3-Category\n 4-Publisher\n 5-Reader\n 6-Exit\n");
+		scanf("%d", &process); 
+		
+		if (process == 6) 
+		{
+			break;
+		}
+		
+		else 
+		{
+			if (process == 1)
+			{
+				printf("Author Process ..... \n");
+			}
+			else if (process == 2)
+			{
+				printf("Book Process ..... \n");
+			}
+			else if (process == 3)
+			{
+				printf("Category Process ..... \n");
+			}
+			else if (process == 4)
+			{
+				printf("Publisher Process ..... \n");
+			}
+			else if (process == 5)
+			{
+				printf("Reader Process ..... \n");
+			}
+			else 
+			{
+				printf("Wrong Choice !!! \n");
+			}
+		}
+	}
 }
