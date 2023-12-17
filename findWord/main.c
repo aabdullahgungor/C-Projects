10a70300c1640dccb6985357975ff5b81e9ba685
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-12-17 23:20:42 +0300

    findWord - main function updated

--- findWord/main.c
+++ findWord/main.c
@@ -2,6 +2,27 @@
 #include <string.h>
 #include <stdlib.h>
 
+void findWord(FILE *file, const char *searchWord) {
+
+}
+
 int main() {
     
+    FILE *file = fopen("text.txt", "r+");
+
+    if (file == NULL) {
+        perror("File opening error");
+        return 1;
+    }
+
+    char word[50];
+
+    printf("Please enter the word to search: ");
+
+    scanf("%s", word);
+
+    findWord(file, word);
+
+    fclose(file);
+
 }
